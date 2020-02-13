2ff3693f46aac8ddc0df7ce1e4fd05bd566eed72
donal <donalspring@gmail.com>
2020-02-13 19:10:08 +0000

    Finished api calls

--- functions/app.js
+++ functions/app.js
@@ -9,8 +9,8 @@ const {
   parse
 } = require('json2csv');
 
-const BASE_URL =
-  'https: //api.github.com/repos/openpracticelibrary/openpracticelibrary/contents/content/practice';
+const BASE_API_URL =
+  'https://api.github.com/repos/springdo/openpracticelibrary/contents/content/practice?ref=master';
 
 let allFilePaths = [];
 
@@ -45,40 +45,51 @@ const someurls = [
 ];
 
 router.get('/data', (req, res) => {
-  // allFilePaths.push(axios.get(`https://api.github.com/repos/openpracticelibrary/openpracticelibrary/contents/content/practice/${element.name}`, options))
-  someurls.forEach(name => {
-    allFilePaths.push(axios.get(`${name}`, options));
-  });
-  axios
-    .all(allFilePaths)
-    .then(allFileContents => {
-      let response = [];
-      allFileContents.forEach(item => {
-        const fileContent = new Buffer(item.data.content, 'base64').toString(
-          'ascii'
-        );
-        let content = fm(fileContent);
-        // trim extra stuff from the metadata
-        delete content.attributes.date;
-        delete content.attributes.authors;
-        delete content.attributes.icon;
-        delete content.attributes.jumbotron;
-        delete content.attributes.jumbotronAlt;
-        delete content.attributes.perspectives;
-        content.attributes.participants = content.attributes.participants ?
-          content.attributes.participants.join('#') :
-          '';
-        // add url
-        // TODO - FIX URL with correct filename
-        content.attributes.url = `https://openpracticelibrary.com/practice/${content.attributes.title}`;
-        response.push(content.attributes);
-      });
-      // TODO - add parser for CSV
-      const csv = parse(response, opts);
-      console.log(csv);
-      req.query.type == 'json' ?
-        res.header('Content-Type', 'application/json') : res.header('Content-Type', 'text/csv');
-      req.query.type == 'json' ? res.status(200).send(response) : res.status(200).send(csv);
+
+  let allFileLocations = [];
+  axios.get(BASE_API_URL, options).then(fileLocations => {
+      console.log(fileLocations)
+      allFileLocations = fileLocations.data;
+      allFileLocations.forEach(file => {
+        allFilePaths.push(axios.get(`${file.url}`, options));
+      })
+      axios
+        .all(allFilePaths)
+        .then(allFileContents => {
+          let response = [];
+          allFileContents.forEach((item, index) => {
+            const fileContent = new Buffer(item.data.content, 'base64').toString(
+              'ascii'
+            );
+            let content = fm(fileContent);
+            // trim extra stuff from the metadata
+            delete content.attributes.date;
+            delete content.attributes.authors;
+            delete content.attributes.icon;
+            delete content.attributes.jumbotron;
+            delete content.attributes.jumbotronAlt;
+            delete content.attributes.perspectives;
+            content.attributes.participants = content.attributes.participants ?
+              content.attributes.participants.join('#') :
+              '';
+            // add url
+            // TODO - FIX URL with correct filename
+            content.attributes.url = `${allFileLocations[index].html_url}`;
+            response.push(content.attributes);
+          });
+          // TODO - add parser for CSV
+          const csv = parse(response, opts);
+          console.info(csv);
+          req.query.type == 'json' ?
+            res.header('Content-Type', 'application/json') : res.header('Content-Type', 'text/csv');
+          req.query.type == 'json' ? res.status(200).send(response) : res.status(200).send(csv);
+        })
+        .catch(error => {
+          // return error
+          console.log(error);
+          res.send(error);
+        });
+
     })
     .catch(error => {
       // return error
@@ -86,7 +97,6 @@ router.get('/data', (req, res) => {
       res.send(error);
     });
 
-  // allFilePaths.push(axios.get(`https://api.github.com/repos/openpracticelibrary/openpracticelibrary/contents/content/practice/${element.name}`, options))
 });
 
 app.use('/.netlify/functions/app', router); // path must route to lambda
